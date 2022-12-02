2a11064b3b12b790149b9270b56075bfe5f0c5d9
sreeharisangeeth <sreeharisangeeth@gmail.com>
2022-12-02 09:39:07 +0530

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sample';
+  screenValue: string = '0';
+  changeValue(input: string) {
+    if(this.screenValue=="0")
+    {
+      this.screenValue=input;
+    }
+    else
+    {
+      this.screenValue = this.screenValue + input;
+    }
+  }
+  backspace() {
+    this.screenValue=this.screenValue.toString();
+    this.screenValue = this.screenValue.slice(0,-1);
+    if(this.screenValue.length==0)
+    {
+      this.screenValue='0';
+    }
+  }
+  calculateResult() {
+    this.screenValue=eval(this.screenValue);
+  }
 }
